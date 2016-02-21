69b6269f82f2bcba79d8416a77b1aedfa9e49c43
Oskar Jermakowicz <oskar.jermakowicz@gmail.com>
2016-02-21 14:16:55 +0100

    Exercise 2 done

--- exercise2/editor.cc
+++ exercise2/editor.cc
@@ -5,5 +5,39 @@
 using namespace std;
 
 string::size_type Editor::find_left_par(string::size_type pos) const {
-	return 0;
+	char rpar = text[pos];
+	char lpar;
+
+	switch (rpar) {
+	case ')' :
+		lpar = '('; 
+		break;
+	case ']' : 
+		lpar = '['; 
+		break;
+	case '}' : 
+		lpar = '{'; 
+		break;
+	default : 
+		return string::npos;
+	}
+
+	int level = 0;
+	string::size_type i = pos - 1;
+	while (i != string::npos) {
+		char ch = text[i];
+		if (ch == lpar && level == 0) {
+			return i;
+		}
+		else {
+			if (ch == rpar) {
+				++level;
+			}
+			else if (ch == lpar) {
+				--level;
+			}
+			--i;
+		}
+	}
+	return i;
 }
