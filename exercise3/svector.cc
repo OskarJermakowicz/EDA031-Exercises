bf049852664ead152420322f86059b38904f6583
Oskar Jermakowicz <oskar.jermakowicz@gmail.com>
2016-02-21 15:32:33 +0100

    svector.cc in exercise 3 done

--- exercise3/svector.cc
+++ exercise3/svector.cc
@@ -6,6 +6,17 @@
 
 SVector::SVector(size_t size) : n(size), v(new int[n]) {}
 
+/** Since the object is moved when calling the method print() in svectortest.cc we need 
+    to implement a copy constructor */
+SVector::SVector(const SVector& sv) : n(sv.n), v(new int[sv.n]) {
+	for (size_t i = 0; i != n; ++i) {
+		v[i] = sv.v[i];
+	}
+}
+
+/** When using operators (for example '='), the same error may occur, in that case we 
+	need to overload operators. I.e SVector& SVector::operator=(const SVector& rhs) */
+
 SVector::~SVector() {
 	delete[] v;
 }
