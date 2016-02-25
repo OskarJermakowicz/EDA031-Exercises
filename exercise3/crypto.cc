ffe6cd86938a308113defe60eec992054c4de7ea
Oskar Jermakowicz <oskar.jermakowicz@gmail.com>
2016-02-25 15:11:06 +0100

    crypto.cc in exercise 3 done

--- exercise3/crypto.cc
+++ exercise3/crypto.cc
@@ -6,9 +6,25 @@
 using namespace std;
 
 string Crypto::encrypt(const string& text, unsigned int key) {
-	return "";
+	string encryptedtext(text.size(), ' ');
+	default_random_engine generator(key);
+	uniform_int_distribution<int> distribution(1, 256);
+
+	for (int i = 0; i < text.length(); ++i) {
+		encryptedtext[i] = (text[i] + distribution(generator)) % 256;
+	}
+
+	return encryptedtext;
 }
 
 string Crypto::decrypt(const string& crypt, unsigned int key) {
-	return "";
+	string decryptedtext(crypt.size(), ' ');
+	default_random_engine generator(key);
+	uniform_int_distribution<int> distribution(1, 256);
+
+	for (int i = 0; i < crypt.length(); ++i) {
+		decryptedtext[i] = (crypt[i] - distribution(generator)) % 256;
+	}
+
+	return decryptedtext;
 }
